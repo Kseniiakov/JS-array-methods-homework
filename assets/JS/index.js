522c9dfa21b618c755a3cc430859cf2d54b4ec3f
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-02-09 22:54:20 +0200

    Add 3 tasks

---
+++ assets/JS/index.js
@@ -0,0 +1,47 @@
+// 1. Массив чисел от 5 до 10 добавить в конец другого массива
+
+// const oldArray = [1,2,3,4];
+
+// const newArray = [5,6,7,8,9,10];
+
+// oldArray.push(newArray);
+
+// console.log(oldArray);
+
+// var 2
+
+// const oldArray = [1,2,3,4];
+
+// const newArray = [5,6,7,8,9,10];
+
+// const resultArray = oldArray.concat(newArray);
+
+// console.log(resultArray);
+
+// 2. Дан массив чисел [4, 9, 16, 25, 36, 49]. 
+// На его основе создать массив с квадратными корнями чисел (Math.sqrt)
+
+// const arr = [4, 9, 16, 25, 36, 49];
+
+// const arrSqrt = [];
+
+// arr.forEach(function(num) {
+//     let sqrt = Math.sqrt(num);
+//     arrSqrt.push(sqrt);
+// });
+
+// console.log(arrSqrt);
+
+// 3. Дан массив чисел 1...20. Вычислить сумму нечетных чисел массива
+
+// const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+// let result = 0;
+
+// arr.forEach(function(num) {
+//     if(num % 2 !== 0) {
+//         result += num;
+//     }
+// })
+
+// console.log(result);
